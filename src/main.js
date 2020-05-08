77900b8ee98dfbe167f1486c7743aad4d803b0c7
chengzi6821 <chengzi6821@163.com>
2020-05-08 23:12:33 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: Dai Junchao
+ * @Date: 2020-05-08 22:22:53
+ * @LastEditors: Dai Junchao
+ * @LastEditTime: 2020-05-08 23:00:46
+ */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
@@ -8,8 +16,8 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
 })
